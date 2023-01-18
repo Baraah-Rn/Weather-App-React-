08c7e2162c144dcbfd7e08c4cd6b5ce1fcd93fa8
Baraah-Rn <baraahranneh@gmail.com>
2023-01-18 11:25:29 +0100

    start to write the useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import "./App.css";
+import { useEffect, useState} from "react";
 import UilReact from "@iconscout/react-unicons/icons/uil-react";
 import TopButton from "./components/TopButtons";
 import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TempAndDetails from "./components/TempAndDetails"
 import Forecast from "./components/Forecast";
-  
+import getFormattedWeatherData from "./services/WeatherServices"
 
 function App() {
+const [query, setQuary]= useState({q: 'berlin'})
+const [unit, setUnits]=useState('metric');
+const [weather,setWeather] = useState(null)
+
+
+useEffect(()=>{
+  const fetchWeather = async()=>{
+    const data = await getFormattedWeatherData({q:"london"})
+    console.log(data)
+   }
+   
+   fetchWeather()
+},[])
+
+
+
+
+
+
   return (
     <div
       className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 
@@ -17,7 +37,8 @@ function App() {
       <Inputs />
       <TimeAndLocation />
       <TempAndDetails/>
-      <Forecast/>
+      <Forecast title= "Hourly forecast "/>
+      <Forecast title= "Daily forecast "/>
     </div>
   );
 }
--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Forecast = () => {
+const Forecast = ({title}) => {
   return (
     <div>
       <div className="flex items-center justify-start mt-6">
-        <p className="text-white font-medium uppercase">title</p>
+        <p className="text-white font-medium uppercase">{title}</p>
       </div>
-      
+
       <hr className="my-2" />
 
       <div className="flex flex-row items-center justify-between text-white">
